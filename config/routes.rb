da54f3ec15da09255dc7b41f991af3bb097597eb
Maria Wissler <wmcarmelina@gmail.com>
2019-05-13 21:07:26 -0700

    added test for index, updated route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   #added
   get "/auth/github", as: "github_login"
-  get "/auth/:provider/callback", to: "users#create"
+  get "/auth/:provider/callback", to: "users#create", as: "auth_callback"
   delete "/logout", to: "users#destroy", as: "logout"
 
   resources :works
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,12 @@
 require "test_helper"
 
 describe UsersController do
+    describe "index" do
+        it "should get index" do
+          # Act
+          get users_path
+          # Assert
+          must_respond_with :success
+        end
+      end 
 end
