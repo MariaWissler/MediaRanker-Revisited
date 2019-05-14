2117b021f0e6dec3ae9f8d1fa7d7525b009bbdb2
Maria Wissler <wmcarmelina@gmail.com>
2019-05-13 21:11:37 -0700

    added test to user_controllers show

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,12 +1,21 @@
 require "test_helper"
 
 describe UsersController do
-    describe "index" do
-        it "should get index" do
-          # Act
-          get users_path
-          # Assert
-          must_respond_with :success
-        end
-      end 
+  describe "index" do
+    it "should get index" do
+      # Act
+      get users_path
+      # Assert
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "can get a valid user" do
+      # Act
+      get user_path(users(:kari).id)
+      # Assert
+      must_respond_with :success
+    end
+  end
 end
